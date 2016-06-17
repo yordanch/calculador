1dc0b5ee2005cb2263d3d63a764322152c9315df
mabhe <maria@gmail.com>
2016-06-17 17:50:19 +0200

    division por cero resuelto

---
+++ calculador/src/calculador/Calculador.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculador;
+
+/**
+ *
+ * @author EPIS
+ */
+public class Calculador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    float sumar(float a, float b){
+    return a+b;
+    } 
+    float resta(float a, float b){
+        return a-b;
+    }
+    float producto(float a, float b){
+        return a * b;
+    }
+     float division(float a, float b){
+         if (b==0) {
+             System.out.print("no se puede dividir");
+             return -1;
+         }
+        return a / b;
+    }
+}
